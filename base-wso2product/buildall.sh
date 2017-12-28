920edc116952fa7df65c7a496c7c3e30f92c6d9e
dlukyanov <dlukyanov@ukr.net>
2017-12-28 12:56:55 +0200

    disable alpine builds for now because libsnappyjava.so in wso2 products is not compatible with alpine/java8

--- base-wso2product/buildall.sh
+++ base-wso2product/buildall.sh
@@ -31,30 +31,34 @@ docker push eleks/base-${P}
 
 echo "ubuntu SUCCESS"
 
-echo "build 4 alpine"
 
-P=wso2bps-3.6.0
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+### disable alpine for now because for example 
+### wso2is-analytics not working correctly because of the snappyjava.so lib in it is not compatible with alpine
 
-P=wso2bps-3.5.1
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+# echo "build 4 alpine"
 
-P=wso2esb-4.9.0
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+# P=wso2bps-3.6.0
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
 
-P=wso2esb-5.0.0
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+# P=wso2bps-3.5.1
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
 
-P=wso2is-5.3.0
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+# P=wso2esb-4.9.0
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
 
-P=wso2is-analytics-5.3.0
-docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
-docker push eleks/alpine-${P}
+# P=wso2esb-5.0.0
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
 
-echo "ubuntu SUCCESS"
+# P=wso2is-5.3.0
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
+
+# P=wso2is-analytics-5.3.0
+# docker build -t eleks/alpine-${P} -f Dockerfile-alpine --build-arg WSO2_PRODUCT=${P} .
+# docker push eleks/alpine-${P}
+
+# echo "ubuntu SUCCESS"
