8d59bb52bc32c37ee51fc1db7ef88d5ec7789ddf
dmytro.lukyanov <dmytru.lukyanov@eleks.com>
2018-02-15 20:18:54 +0200

    fix unzip for archives created on windowz with `/` as file separator

--- base-jdk8-groovy/src/opt/tools/classes/Tools.groovy
+++ base-jdk8-groovy/src/opt/tools/classes/Tools.groovy
@@ -8,12 +8,14 @@ public class Tools {
 	public static void unzip(File src, Path dst, boolean verbose=false){
 		def zipFile = new ZipFile(src)
 		zipFile.entries().each { ZipEntry entry->
-			Path dstUnzip = dst.resolve(entry.getName())
-			if(entry.isDirectory()){
-				dstUnzip.toFile().mkdirs()
+			def entryName = entry.getName().tr('\\','/')
+			File dstUnzip = new File(dst.toFile(), entryName)
+			if(entryName.endsWith('/')){
+				dstUnzip.mkdirs()
 			}else{
 				if(verbose)println "     [entry] ${dstUnzip}"
-				dstUnzip.toFile().withOutputStream{ it << zipFile.getInputStream(entry) }
+				dstUnzip.getParentFile().mkdirs()
+				dstUnzip.withOutputStream{ it << zipFile.getInputStream(entry) }
 			}
 		}
 		zipFile.close()
